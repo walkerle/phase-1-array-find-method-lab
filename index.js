f6fafb88f34a858cbfc678ca71a5c73ed04811e6
Walker Le <walker.le88@gmail.com>
2022-12-28 13:47:39 -0700

    commit tree 811aa81332b3051ccfda97d1f54eb0e804db497c

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code your solution here
+
+// const record = [ // Example test array
+//     {year: "2015", result: "W"},
+//     {year: "2014", result: "N/A"},
+//     {year: "2013", result: "L"}
+// ]
+
+function superbowlWin(array) {
+    const result = array.find(objectElement => {
+        return objectElement.result === "W";
+    });
+    // console.log(result);
+    return (result ? result.year : undefined)
+}
+
+// console.log(superbowlWin(record));
+
+// function check(objElement) {
+//     const result = objElement.result === "W";
+//     // console.log(result);
+//     // console.log(objElement);
+//     // console.log(objElement.year);
+//     const year = (result ? objElement.year : undefined)
+//     return year
+// }
+
+// record.find(check); // Remember Array.find ONLY returns a true or false!! SO this does not work for the lab!
+// console.log(record.find(check));
